d11c4061888ae780abf7aa73a37d53b299910ea9
Karthik Kumar Mishra <kartik.kumar.mishra0905@gmail.com>
2025-04-30 17:20:36 +0530

    Updated package files

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 import express from "express";
 
 const app = express();
-const PORT = process.env.PORT;
 
 app.use(express.static("dist")); // Assuming Vite's build output is stored in `dist`
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const PORT = process.env.PORT || 10000; // Default Render port
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
